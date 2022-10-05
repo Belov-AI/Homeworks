49b1aa64f3901af6a3a334b02ede7320c6a1767c
Belov-AI <Belov.Alexander@urfu.ru>
2022-10-05 20:13:52 +0500

    Task02 comleted

---
+++ Task02/Task02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус круга");
+            double r = double.Parse(Console.ReadLine());
+
+            double s = Math.PI * r * r;
+
+            Console.WriteLine("Площадь круга: " + s);
+
+            Console.ReadKey();
+        }
+    }
+}
